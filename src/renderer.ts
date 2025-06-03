d549092b4eff1e920a5bc379da43712bb460f7b6
jktham <jonas.tham@gmx.ch>
2025-06-03 17:45:31 +0200

    set up vertex and uniform buffers

--- src/renderer.ts
+++ src/renderer.ts
@@ -8,6 +8,10 @@ export class Renderer {
     private fragmentShader!: GPUShaderModule
     private pipeline!: GPURenderPipeline
     private renderPassDescriptor!: GPURenderPassDescriptor
+    private vertexBuffer!: GPUBuffer
+    private uniformBuffer!: GPUBuffer
+    private uniformData!: Float32Array
+    private bindGroup!: GPUBindGroup
 
     constructor(private canvas: HTMLCanvasElement) {
 		
@@ -18,6 +22,8 @@ export class Renderer {
         this.configCanvas()
         this.loadShaders()
         this.configurePipeline()
+        this.configureVertexBuffer()
+        this.configureUniformBuffer()
         this.configureRenderPassDescriptor()
         this.render()
     }
@@ -75,7 +81,18 @@ export class Renderer {
         this.pipeline = this.device.createRenderPipeline({
             label: "Render Pipeline",
             layout: "auto",
-            vertex: { module: this.vertexShader },
+            vertex: {
+                module: this.vertexShader,
+                buffers: [
+                    {
+                        arrayStride: 6 * 4,
+                        attributes: [
+                            {shaderLocation: 0, offset: 0, format: "float32x2"}, // pos
+                            {shaderLocation: 1, offset: 2 * 4, format: "float32x4"}, // color
+                        ]
+                    }
+                ]
+            },
             fragment: {
                 module: this.fragmentShader,
                 targets: [{ format: this.presentationFormat }]
@@ -83,11 +100,45 @@ export class Renderer {
         })
     }
 
+    private configureVertexBuffer() {
+        const vertexData = new Float32Array(6 * 3)
+        vertexData.set([
+             0.0,  0.8, 1.0, 0.0, 0.0, 1.0,
+            -0.8, -0.8, 0.0, 1.0, 0.0, 1.0,
+             0.8, -0.8, 0.0, 0.0, 1.0, 1.0,
+        ])
+        
+        this.vertexBuffer = this.device.createBuffer({
+            label: "vertex buffer",
+            size: vertexData.byteLength,
+            usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
+        })
+        this.device.queue.writeBuffer(this.vertexBuffer, 0, vertexData)
+    }
+
+    private configureUniformBuffer() {
+        this.uniformBuffer = this.device.createBuffer({
+            label: "uniform buffer",
+            size: 4,
+            usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
+        })
+        this.uniformData = new Float32Array(1)
+        this.uniformData[0] = 0.0
+
+        this.bindGroup = this.device.createBindGroup({
+            layout: this.pipeline.getBindGroupLayout(0),
+            entries: [
+                { binding: 0, resource: { buffer: this.uniformBuffer }},
+            ],
+      });
+
+    }
+
     private configureRenderPassDescriptor() {
         this.renderPassDescriptor = {
             label: "Render Pass Description",
             colorAttachments: [{
-                clearValue: [0.0, 0.8, 0.0, 1.0],
+                clearValue: [0.0, 0.0, 0.0, 1.0],
                 loadOp: "clear",
                 storeOp: "store",
                 view: this.context.getCurrentTexture().createView()
@@ -98,11 +149,15 @@ export class Renderer {
     private render() {
         (this.renderPassDescriptor.colorAttachments as any)[0].view = this.context.getCurrentTexture().createView()
 
-        const encoder = this.device.createCommandEncoder({ label: "render encoder" })
+        this.uniformData[0] = ((Date.now() - 1748964096000) / 1000)
+        this.device.queue.writeBuffer(this.uniformBuffer, 0, this.uniformData)
 
+        const encoder = this.device.createCommandEncoder({ label: "render encoder" })
         const pass = encoder.beginRenderPass(this.renderPassDescriptor)
         pass.setPipeline(this.pipeline)
-        pass.draw(6)
+        pass.setVertexBuffer(0, this.vertexBuffer)
+        pass.setBindGroup(0, this.bindGroup)
+        pass.draw(3)
         pass.end()
 
         this.device.queue.submit([encoder.finish()])
--- src/shaders.ts
+++ src/shaders.ts
@@ -1,28 +1,23 @@
 export const vert = /* wgsl */ `
+struct VertexIn {
+	@location(0) pos: vec2f,
+	@location(1) color: vec4f
+};
+
 // data structure to store output of vertex function
 struct VertexOut {
 	@builtin(position) pos: vec4f,
 	@location(0) color: vec4f
 };
 
+@group(0) @binding(0) var<uniform> time: f32;
+
 // process the points of the triangle
 @vertex 
-fn vs(@builtin(vertex_index) vertexIndex : u32) -> VertexOut {
-	let pos = array(
-		vec2f( 0.0,  0.8),  // top center
-		vec2f(-0.8, -0.8),  // bottom left
-		vec2f( 0.8, -0.8)   // bottom right
-	);
-
-	let color = array(
-		vec4f(1.0, 0.0, 0.0, 1.0),
-		vec4f(0.0, 1.0, 0.0, 1.0),
-		vec4f(0.0, 0.0, 1.0, 1.0)
-	);
-
+fn vs(vert: VertexIn) -> VertexOut {
 	var out: VertexOut;
-	out.pos = vec4f(pos[vertexIndex], 0.0, 1.0);
-	out.color = color[vertexIndex];
+	out.pos = vec4f(vert.pos, 0.0, 1.0);
+	out.color = vert.color * fract(time);
 
 	return out;
 }
