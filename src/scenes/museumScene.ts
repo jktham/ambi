e6b9eed488d739f8106c1dea126395253ec134a5
jktham <jonas.tham@gmx.ch>
2025-11-18 18:23:49 +0100

    adjust museum lighting

--- src/scenes/museumScene.ts
+++ src/scenes/museumScene.ts
@@ -1,7 +1,7 @@
 import { engine } from "../main";
 import { Scene, WorldObject } from "../scene";
 import { Trigger } from "../trigger";
-import { PostOutlineUniforms } from "../uniforms";
+import { PhongUniforms, PostOutlineUniforms } from "../uniforms";
 import { Mat4, Vec2, Vec3, Vec4 } from "../vec";
 
 export class MuseumScene extends Scene {
@@ -15,27 +15,31 @@ export class MuseumScene extends Scene {
 		super();
 
 		this.postUniforms.scale.fill(2);
-		this.postUniforms.mode[0] = 1;
-		this.postUniforms.color[0] = new Vec4(0, 0, 0, 1);
-		this.postUniforms.mode[1] = 1;
-		this.postUniforms.color[1] = new Vec4(0, 0, 0, 1);
-		this.postUniforms.mode[2] = 1;
-		this.postUniforms.color[2] = new Vec4(0, 0, 0, 1);
+		this.postUniforms.mode.fill(1);
+		this.postUniforms.color = this.postUniforms.color.map(_ => new Vec4(0, 0, 0, 1));
 	}
 	
 	init() {
+		let phong = new PhongUniforms();
+		phong.lightPos = new Vec3(400, 1200, 800);
+		phong.ambientFactor = 0.8;
+		phong.diffuseFactor = 0.2;
+		phong.specularFactor = 0.0;
+
 		let obj = new WorldObject();
 		obj.mesh = "museum/room.obj";
 		obj.collider = "museum/room.obj";
 		obj.textures[0] = "blank.png";
 		obj.mask = 0;
+		obj.fragShader = "world/phong.frag.wgsl";
+		obj.fragUniforms = phong;
 		this.objects.push(obj);
 
 		let scenes = ["debug", "pier", "brutal"];
 		let positions = [
-			new Vec3(-5, 0, -13),
-			new Vec3(0, 0, -13),
-			new Vec3(5, 0, -13),
+			new Vec3(-5, -0.01, -13),
+			new Vec3(0, -0.01, -13),
+			new Vec3(5, -0.01, -13),
 		];
 		let colors = [
 			new Vec4(1, 1, 1, 1),
@@ -47,6 +51,7 @@ export class MuseumScene extends Scene {
 			obj = new WorldObject();
 			obj.model = Mat4.translate(positions[i]);
 			obj.mesh = "museum/door.obj";
+			// obj.textures[0] = `doors/${scenes[i]}.png`;
 			obj.textures[0] = "blank.png";
 			obj.color = colors[i];
 			obj.fragShader = "world/noise.frag.wgsl";
@@ -59,6 +64,8 @@ export class MuseumScene extends Scene {
 			obj.collider = "museum/doorframe.obj";
 			obj.textures[0] = "blank.png";
 			obj.mask = 2;
+			obj.fragShader = "world/phong.frag.wgsl";
+			obj.fragUniforms = phong;
 			this.objects.push(obj);
 
 			let t = new Trigger();
